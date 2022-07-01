064739e89b5cce43ca0913edc722093e6f045efa
Erick Paiva <123skatesk8@gmail.com>
2022-06-30 21:27:03 -0400

    feat: I added the router browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
-
-const container = document.getElementById("root")
-if (!container) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(container)
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
-  </React.StrictMode>,
-)
-
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
