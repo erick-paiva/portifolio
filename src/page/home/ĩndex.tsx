c9f5548c5cbced9f6e09772ef61fb3497c38be31
Erick Paiva <123skatesk8@gmail.com>
2022-06-30 21:07:01 -0400

    feat: I added the header in the app

--- src/page/home/ĩndex.tsx
+++ src/page/home/ĩndex.tsx
@@ -1,17 +1,21 @@
 import { Center, Heading } from "@chakra-ui/react";
+import Header from "../../components/header";
 
-interface HomeProps {
-    
-}
- 
+interface HomeProps {}
+
 const Home = ({}: HomeProps) => {
-    return (
-        <Center>
-            <Heading>
-                home
-            </Heading>
-        </Center>
-    );
-}
- 
+  return (
+    <Center
+      w="100vw"
+      h="100vh"
+      bg="blue"
+      flexDirection="column"
+      justifyContent="flex-start"
+    >
+      <Header />
+      <Heading>home</Heading>
+    </Center>
+  );
+};
+
 export default Home;
