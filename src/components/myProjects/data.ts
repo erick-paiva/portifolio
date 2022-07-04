12c16e54a4586d65a023836eb25d59ac1f7d1398
Erick Paiva <123skatesk8@gmail.com>
2022-07-04 11:29:15 -0400

    feat: I added some projects

---
+++ src/components/myProjects/data.ts
@@ -0,0 +1,26 @@
+export const myProjects = [
+  {
+    name: "Follow Events",
+    repo: "https://github.com/followevents-corp/follow-events-backend",
+    description:
+      "Folllow events é uma plataforma de agendamento de eventos voltada ao mercado de Live Stream, facilitando ao usuário encontrar o que deseja e o produtor de conteúdo a destacar seus eventos e criar seus eventos de sorte pra atrair ou recompensar a audiência.",
+  },
+  {
+    name: "Follow Events",
+    repo: "https://github.com/followevents-corp/follow-events-backend",
+    description:
+      "Folllow events é uma plataforma de agendamento de eventos voltada ao mercado de Live Stream, facilitando ao usuário encontrar o que deseja e o produtor de conteúdo a destacar seus eventos e criar seus eventos de sorte pra atrair ou recompensar a audiência.",
+  },
+  {
+    name: "Follow Events",
+    repo: "https://github.com/followevents-corp/follow-events-backend",
+    description:
+      "Folllow events é uma plataforma de agendamento de eventos voltada ao mercado de Live Stream, facilitando ao usuário encontrar o que deseja e o produtor de conteúdo a destacar seus eventos e criar seus eventos de sorte pra atrair ou recompensar a audiência.",
+  },
+  {
+    name: "Follow Events",
+    repo: "https://github.com/followevents-corp/follow-events-backend",
+    description:
+      "Folllow events é uma plataforma de agendamento de eventos voltada ao mercado de Live Stream, facilitando ao usuário encontrar o que deseja e o produtor de conteúdo a destacar seus eventos e criar seus eventos de sorte pra atrair ou recompensar a audiência.",
+  },
+];
