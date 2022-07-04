fcc484f4e64aa9dbb200d6ad5d0d25d55e8d7c00
Erick Paiva <123skatesk8@gmail.com>
2022-07-04 19:43:52 -0400

    feat: I added the theme in the application index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { theme } from "./styles";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
