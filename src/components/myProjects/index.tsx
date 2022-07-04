8324de2c022c74fa92d878e9874e2c762cdfba17
Erick Paiva <123skatesk8@gmail.com>
2022-07-03 21:29:47 -0400

    feat: I created the base for the my projects session

---
+++ src/components/myProjects/index.tsx
@@ -0,0 +1,9 @@
+import { Center } from "@chakra-ui/react";
+
+interface MyProjectsProps {}
+
+const MyProjects = ({}: MyProjectsProps) => {
+  return <Center w="100%" minH="100px" bg="white" mt="50px"></Center>;
+};
+
+export default MyProjects;
