2f4596c34e046198ca7d1671b3328712838cbab9
Erick Paiva <123skatesk8@gmail.com>
2022-07-05 18:52:34 -0400

    feat: I added the session for backend projects

--- src/components/myProjects/index.tsx
+++ src/components/myProjects/index.tsx
@@ -1,23 +1,12 @@
-import {
-  Box,
-  Center,
-  Flex,
-  Heading,
-  Img,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Img, Text } from "@chakra-ui/react";
 import ImgFront from "../../assets/home/front-end.png";
 import ImgBack from "../../assets/home/back-end.png";
-import { myProjects } from "./data";
+import { myProjectsFront } from "./data";
 import CardMyProjects from "../cardMyProjects";
 interface MyProjectsProps {
-  area?: string;
 }
 
-const MyProjects = ({ area = "front-end" }: MyProjectsProps) => {
-  const img = area === "front-end" ? ImgFront : ImgBack;
-  const name = area === "front-end" ? "Front-end" : "Back-end";
-
+const MyProjects = ({}: MyProjectsProps) => {
   return (
     <Flex
       w="100%"
@@ -27,6 +16,7 @@ const MyProjects = ({ area = "front-end" }: MyProjectsProps) => {
       borderRadius="10px"
       flexDirection="column"
       padding="10px 15px"
+      mb="480px"
     >
       <Heading fontSize="25px">Some of my projects</Heading>
       <Center
@@ -38,12 +28,37 @@ const MyProjects = ({ area = "front-end" }: MyProjectsProps) => {
         border="2px solid black"
         my="15px"
       >
-        <Text>{name}</Text>
-        <Img src={img} alt={name} h="20px" w="20px" ml="10px" />
+        <Text>Front-end</Text>
+        <Img src={ImgFront} alt="Front-end" h="20px" w="20px" ml="10px" />
       </Center>
 
       <Center flexWrap="wrap" justifyContent="space-between">
-        {myProjects.map(({ name, repo, description }, index) => (
+        {myProjectsFront.map(({ name, repo, description }, index) => (
+          <Box
+            ml={(index + 1) % 2 === 0 ? "10px" : "0"}
+            mb="10px"
+            key={name + index}
+          >
+            <CardMyProjects name={name} link={repo} description={description} />
+          </Box>
+        ))}
+      </Center>
+
+      <Center
+        px="10px"
+        w="130px"
+        color="white"
+        bg="blue"
+        borderRadius="5px"
+        border="2px solid black"
+        my="15px"
+      >
+        <Text>Back-end</Text>
+        <Img src={ImgBack} alt="Back-end" h="20px" w="20px" ml="10px" />
+      </Center>
+
+      <Center flexWrap="wrap" justifyContent="space-between">
+        {myProjectsFront.map(({ name, repo, description }, index) => (
           <Box
             ml={(index + 1) % 2 === 0 ? "10px" : "0"}
             mb="10px"
