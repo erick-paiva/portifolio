b9cf61a0b755a7dd7d905a84957366fab23405f2
Erick Paiva <123skatesk8@gmail.com>
2022-07-04 22:47:53 -0400

    feat: I added the social media data

---
+++ src/components/footer/data.ts
@@ -0,0 +1,16 @@
+import Github from "../../assets/home/github.png";
+import Linkedin from "../../assets/home/linkedin.png";
+import Facebook from "../../assets/home/facebook.png";
+import Instagram from "../../assets/home/instagram.png";
+import Gmail from "../../assets/home/gmail.png";
+
+export const socialmedias = [
+  { img: Github, link: "https://github.com/erick-paiva" },
+  { img: Linkedin, link: "https://www.linkedin.com/in/erick-paiva-silva/" },
+  { img: Facebook, link: "https://www.facebook.com/erick.paiva.90260/" },
+  { img: Instagram, link: "https://www.instagram.com/erickpsdev/" },
+  {
+    img: Gmail,
+    link: "mailto: erickpaivaasilva@gmail.com",
+  },
+];
