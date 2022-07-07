97837a852783a711349abee47e73e70dcbfb53e5
Erick Paiva <123skatesk8@gmail.com>
2022-07-06 22:02:37 -0400

    feat: I created the front and back technologies data

---
+++ src/components/myHardSkills/data.ts
@@ -0,0 +1,280 @@
+import Javascript from "../../assets/hard-skills/js.png";
+import Typescript from "../../assets/hard-skills/typescript.png";
+import css from "../../assets/hard-skills/css-3.png";
+import sass from "../../assets/hard-skills/sass.png";
+import html from "../../assets/hard-skills/html5.png";
+import markdown from "../../assets/hard-skills/markdown.png";
+import reactjs from "../../assets/hard-skills/react.png";
+import redux from "../../assets/hard-skills/redux.png";
+import styledComp from "../../assets/hard-skills/sc.png";
+import chakra from "../../assets/hard-skills/chakra-ui.png";
+import material from "../../assets/hard-skills/material-ui.png";
+import reactHookForm from "../../assets/hard-skills/hook-form.jpg";
+import yup from "../../assets/hard-skills/yup.png";
+import reactIcons from "../../assets/hard-skills/react-icons.png";
+import jest from "../../assets/hard-skills/jest.png";
+import cypress from "../../assets/hard-skills/cypress.png";
+import figma from "../../assets/hard-skills/figma.png";
+import vscode from "../../assets/hard-skills/vscode.png";
+import git from "../../assets/hard-skills/git.png";
+import gitHub from "../../assets/hard-skills/github.png";
+import gimp from "../../assets/hard-skills/gimp.png";
+import canvas from "../../assets/hard-skills/canva.png";
+import adobeXd from "../../assets/hard-skills/adober-color.png";
+import insomnia from "../../assets/hard-skills/insomnia.png";
+import heroku from "../../assets/hard-skills/heroku.png";
+import vercel from "../../assets/hard-skills/vercel.png";
+import python from "../../assets/hard-skills/python.png";
+import sqlAlchemy from "../../assets/hard-skills/sqlalchemy.png";
+import flask from "../../assets/hard-skills/flask.png";
+import node from "../../assets/hard-skills/node.png";
+import swagger from "../../assets/hard-skills/swagger.png";
+import pytest from "../../assets/hard-skills/pytest.png";
+import faker from "../../assets/hard-skills/faker.png";
+import typeOrm from "../../assets/hard-skills/typeorm.png";
+import docker from "../../assets/hard-skills/docker.png";
+
+export const myHardSkillsDataFront = [
+  {
+    title: "Languages",
+    data: [
+      {
+        img: Javascript,
+        name: "Javascript",
+      },
+      {
+        img: Typescript,
+        name: "Javascript",
+      },
+    ],
+  },
+  {
+    title: "Styling Languages",
+    data: [
+      {
+        img: css,
+        name: "Css",
+      },
+      {
+        img: sass,
+        name: "Sass",
+      },
+    ],
+  },
+  {
+    title: "Markup Languages",
+    data: [
+      {
+        img: html,
+        name: "Html 5",
+      },
+      {
+        img: markdown,
+        name: "Markdown",
+      },
+    ],
+  },
+  {
+    title: "Libraries",
+    data: [
+      {
+        img: reactjs,
+        name: "React Js",
+      },
+      {
+        img: redux,
+        name: "Redux",
+      },
+      {
+        img: styledComp,
+        name: "Styled components",
+      },
+      {
+        img: chakra,
+        name: "Chakra ui",
+      },
+      {
+        img: material,
+        name: "Material ui",
+      },
+      {
+        img: reactHookForm,
+        name: "React-hook-form",
+      },
+      {
+        img: yup,
+        name: "Yup",
+      },
+      {
+        img: reactIcons,
+        name: "React icons",
+      },
+      {
+        img: jest,
+        name: "Jest",
+      },
+      {
+        img: cypress,
+        name: "Cypress",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    data: [
+      {
+        img: figma,
+        name: "Figma",
+      },
+      {
+        img: vscode,
+        name: "Vscode",
+      },
+      {
+        img: git,
+        name: "Git",
+      },
+      {
+        img: gitHub,
+        name: "GitHub",
+      },
+      {
+        img: gimp,
+        name: "Gimp",
+      },
+      {
+        img: canvas,
+        name: "Canvas",
+      },
+      {
+        img: adobeXd,
+        name: "Adobe xd",
+      },
+      {
+        img: insomnia,
+        name: "Insomnia",
+      },
+    ],
+  },
+  {
+    title: "Cloud",
+    data: [
+      {
+        img: heroku,
+        name: "Heroku",
+      },
+      {
+        img: vercel,
+        name: "Vercel",
+      },
+    ],
+  },
+];
+
+export const myHardSkillsDataBack = [
+  {
+    title: "Languages",
+    data: [
+      {
+        img: Javascript,
+        name: "Javascript",
+      },
+      {
+        img: Typescript,
+        name: "Javascript",
+      },
+      {
+        img: python,
+        name: "Python",
+      },
+    ],
+  },
+  {
+    title: "Markup Languages",
+    data: [
+      {
+        img: markdown,
+        name: "Markdown",
+      },
+    ],
+  },
+  {
+    title: "Libraries",
+    data: [
+      {
+        img: flask,
+        name: "Flask",
+      },
+      {
+        img: node,
+        name: "Node",
+      },
+      {
+        img: jest,
+        name: "Jest",
+      },
+      {
+        img: swagger,
+        name: "Swagger",
+      },
+      {
+        img: pytest,
+        name: "Pytest",
+      },
+      {
+        img: yup,
+        name: "Yup",
+      },
+      {
+        img: faker,
+        name: "Faker",
+      },
+      {
+        img: typeOrm,
+        name: "TypeOrm",
+      },
+      {
+        img: sqlAlchemy,
+        name: "SqlAlchemy",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    data: [
+      {
+        img: vscode,
+        name: "Vscode",
+      },
+      {
+        img: git,
+        name: "Git",
+      },
+      {
+        img: gitHub,
+        name: "GitHub",
+      },
+      {
+        img: insomnia,
+        name: "Insomnia",
+      },
+      {
+        img: docker,
+        name: "Docker",
+      },
+    ],
+  },
+  {
+    title: "Cloud",
+    data: [
+      {
+        img: heroku,
+        name: "Heroku",
+      },
+      {
+        img: vercel,
+        name: "Vercel",
+      },
+    ],
+  },
+];
