c2417ab48b7f8d9f4574cd994e2152d44cdf82a9
Erick Paiva <123skatesk8@gmail.com>
2022-07-06 22:03:47 -0400

    feat: I created the myHardSkills page

---
+++ src/pages/myHardSkills/backgroundEffect.js
@@ -0,0 +1,138 @@
+export const effect = () => {
+  let canvas = document.getElementById("backgroundEffect");
+
+  let w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight),
+    ctx = canvas.getContext("2d"),
+    minDist = 10,
+    maxDist = 30,
+    initialWidth = 10,
+    maxLines = 100,
+    initialLines = 4,
+    speed = 5,
+    lines = [],
+    frame = 0,
+    timeSinceLast = 0,
+    dirs = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0],
+      [0.7, 0.7],
+      [0.7, -0.7],
+      [-0.7, 0.7],
+      [-0.7, -0.7],
+    ],
+    starter = {
+      x: w / 2,
+      y: h / 2,
+      vx: 0,
+      vy: 0,
+      width: initialWidth,
+    };
+
+  function init() {
+    lines.length = 0;
+
+    for (let i = 0; i < initialLines; ++i) lines.push(new Line(starter));
+
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, w, h);
+  }
+  function getColor(x) {
+    return "hsl( hue, 80%, 50% )".replace("hue", (x / w) * 360 + frame);
+  }
+  function anim() {
+    window.requestAnimationFrame(anim);
+
+    ++frame;
+
+    ctx.shadowBlur = 0;
+    ctx.fillStyle = "rgba(0,0,0,.02)";
+    ctx.fillRect(0, 0, w, h);
+    ctx.shadowBlur = 0.5;
+    for (let i = 0; i < lines.length; ++i)
+      if (lines[i].step()) {
+        lines.splice(i, 1);
+        --i;
+      }
+
+    ++timeSinceLast;
+
+    if (lines.length < maxLines && timeSinceLast > 10 && Math.random() < 0.5) {
+      timeSinceLast = 0;
+
+      lines.push(new Line(starter));
+
+      ctx.fillStyle = ctx.shadowColor = getColor(starter.x);
+      ctx.beginPath();
+      ctx.arc(starter.x, starter.y, initialWidth, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+  function Line(parent) {
+    this.x = parent.x | 0;
+    this.y = parent.y | 0;
+    this.width = parent.width / 1.25;
+
+    do {
+      let dir = dirs[(Math.random() * dirs.length) | 0];
+      this.vx = dir[0];
+      this.vy = dir[1];
+    } while (
+      (this.vx === -parent.vx && this.vy === -parent.vy) ||
+      (this.vx === parent.vx && this.vy === parent.vy)
+    );
+
+    this.vx *= speed;
+    this.vy *= speed;
+
+    this.dist = Math.random() * (maxDist - minDist) + minDist;
+  }
+
+  Line.prototype.step = function () {
+    let dead = false;
+
+    let prevX = this.x,
+      prevY = this.y;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    --this.dist;
+
+    if (this.x < 0 || this.x > w || this.y < 0 || this.y > h) dead = true;
+
+    if (this.dist <= 0 && this.width > 1) {
+      this.dist = Math.random() * (maxDist - minDist) + minDist;
+
+      if (lines.length < maxLines) lines.push(new Line(this));
+      if (lines.length < maxLines && Math.random() < 0.5)
+        lines.push(new Line(this));
+
+      if (Math.random() < 0.2) dead = true;
+    }
+
+    ctx.strokeStyle = ctx.shadowColor = getColor(this.x);
+    ctx.beginPath();
+    ctx.lineWidth = this.width;
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(prevX, prevY);
+    ctx.stroke();
+
+    if (dead) return true;
+  };
+
+  init();
+  anim();
+
+  window.addEventListener("resize", function () {
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+    starter.x = w / 2;
+    starter.y = h / 2;
+
+    init();
+  });
+};
---
+++ src/pages/myHardSkills/index.tsx
@@ -0,0 +1,102 @@
+import { Box, Center, keyframes } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
+import MyHardSkills from "../../components/myHardSkills";
+import SessionAboutMe from "../../components/sessionAboutMe";
+import Sidebar from "../../components/sidebar";
+import { effect } from "./backgroundEffect";
+
+interface HomeProps {}
+
+const MyHardSkillsPage = ({}: HomeProps) => {
+  const [openSideBar, setOpenSideBar] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const [offY, setOffY] = useState(0);
+  const handleScroll = () => {
+    setOffY(window.scrollY);
+    if (window.scrollY > 0) {
+      setScroll(true);
+    }
+  };
+  const animation = keyframes`
+  from{
+    filter: blur(10px);
+  }to{
+    filter: blur(0);
+  }
+`;
+
+  const scrollYEffect = {
+    transform: `translateY(${offY * 0.1}px)`,
+    transition: "0.2s ease-in-out",
+  };
+
+  const scrollYEffect05 = {
+    transform: `translateY(${offY * 0.2}px)`,
+    transition: "0.15s ease-in-out",
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (window.scrollY === offY) {
+        setScroll(false);
+      }
+    }, 100);
+  }, [scroll, offY]);
+
+  useEffect(() => {
+    effect();
+  }, []);
+
+  return (
+    <Center
+      w="100vw"
+      minH="100vh"
+      flexDirection="column"
+      justifyContent="flex-start"
+      animation={`${animation} 0.7s ease-in`}
+    >
+      <Sidebar
+        open={openSideBar}
+        callback={() => setOpenSideBar(!openSideBar)}
+      />
+      <Header callback={() => setOpenSideBar(!openSideBar)} />
+      <Box
+        paddingX="5%"
+        paddingY="50px"
+        h="100%"
+        w="100%"
+        onClick={() => setOpenSideBar(false)}
+      >
+        <Box
+          id="backgroundEffect"
+          position="fixed"
+          top={0}
+          left={0}
+          zIndex="-1"
+          w="100%"
+          minH="100%"
+          as="canvas"
+
+        />
+        <Box css={scrollYEffect} w="100%">
+          <SessionAboutMe />
+        </Box>
+
+        <Box w="100%">
+          <MyHardSkills />
+        </Box>
+      </Box>
+      <Footer />
+    </Center>
+  );
+};
+
+export default MyHardSkillsPage;
